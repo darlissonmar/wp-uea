d892fe4eba0cf235888cf4c6a481229a0cdbfa1f
Darlisson Marinho <darlisson11@gmail.com>
2013-09-01 19:59:16 -0400

    [src] Adiciona Solucoes da Lista de Exercicios

---
+++ SRC/ListaWP/ListaWP/BolaPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using ListaWP.Resources;
+
+namespace ListaWP
+{
+    public partial class BolaPage : PhoneApplicationPage
+    {
+        // Constructor
+        public Bola bola;
+
+        public BolaPage()
+        {
+            InitializeComponent();
+            bola = new Bola("White");
+            xamlBola.DataContext = bola;
+            
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        private void btnAzul_Click(object sender, RoutedEventArgs e)
+        {
+
+            bola.Cor = "Blue";
+        }
+
+        private void btnVioleta_Click(object sender, RoutedEventArgs e)
+        {
+            bola.Cor = "Violet";
+        }
+
+        private void btnVermelho_Click(object sender, RoutedEventArgs e)
+        {
+            bola.Cor = "Red";
+        }
+
+        private void btnLaranja_Click(object sender, RoutedEventArgs e)
+        {
+            bola.Cor = "Orange";
+        }
+
+        private void btnVerde_Click(object sender, RoutedEventArgs e)
+        {
+            bola.Cor = "Green";
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
---
+++ SRC/ListaWP/ListaWP/ViewModel/Bola.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ListaWP
+{
+    public class Bola : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Bola(string cor) 
+        {
+            _cor = cor;
+        }
+
+        private string _cor;
+        public string Cor
+        {
+            get { return _cor; }
+            set {
+                    if (value != _cor)
+                        {
+                            _cor = value;
+                            OnPropertyChanged("Cor");
+                        }
+                }
+        }
+
+        private int _cirunferencia;
+        private string _material;
+        
+
+         
+       public void OnPropertyChanged(string propertyName)
+       {
+           var propertyChanged = PropertyChanged;
+           if (propertyChanged != null)
+               {
+                    propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+        }
+    }
+}
